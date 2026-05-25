c19c12eeeb361674549be7913f662d001dad29fd
dhonims2005techac-ai <dhonims2005techac@gmail.com>
2026-05-26 03:31:49 +0530

    Update worker.py

--- worker.py
+++ worker.py
@@ -42,6 +42,6 @@ def compute():
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT",7860 ))
     print(f"Grid node starting on port {port}...")
     app.run(host='0.0.0.0', port=port)
